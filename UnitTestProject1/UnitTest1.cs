e7d9fef1b7ef39776c40da71deb06a047e1dac76
Mladen B <mladen074@gmail.com>
2016-05-20 09:45:40 +0200

    initial commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			//var a = new Mock
+			Assert.IsTrue(false);
+		}
+	}
+}
