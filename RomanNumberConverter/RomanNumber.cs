79e384d7e3c6ebbe75dec36c9d6861303611879c
Mladen B <mladen074@gmail.com>
2016-05-20 11:46:50 +0200

    Finished.

---
+++ RomanNumberConverter/RomanNumber.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumberConverter
+{
+	public class RomanNumber
+	{
+		private readonly int _number;
+
+		private readonly IDictionary<int, string>[] _digitsMap = new IDictionary<int, string>[]
+		{
+			new Dictionary<int, string>
+			{
+				{1, "I"},
+				{2, "II"},
+				{3, "III"},
+				{4, "IV"},
+				{5, "V"},
+				{6, "VI"},
+				{7, "VII"},
+				{8, "VIII"},
+				{9, "IX"},
+			},
+			new Dictionary<int, string>
+			{
+				{1, "X"},
+				{2, "XX"},
+				{3, "XXX"},
+				{4, "XL"},
+				{5, "L"},
+				{6, "LX"},
+				{7, "LXX"},
+				{8, "LXXX"},
+				{9, "XC"},
+			},
+			new Dictionary<int, string>
+			{
+				{1, "C"},
+				{2, "CC"},
+				{3, "CCC"},
+				{4, "CD"},
+				{5, "D"},
+				{6, "DC"},
+				{7, "DCC"},
+				{8, "DCCC"},
+				{9, "CM"},
+			},
+			new Dictionary<int, string>
+			{
+				{1, "M"},
+				{2, "MM"},
+				{3, "MMM"},
+			}
+		};
+
+		public RomanNumber(int number)
+		{
+			if (number < 1 || number > 3999)
+				throw new ArgumentException("number");
+
+			_number = number;
+		}
+
+		public IEnumerable<int> Digits()
+		{
+			var number = _number;
+
+			while (number > 0)
+			{
+				var digit = number % 10;
+				number /= 10;
+				yield return digit;
+			}
+		} 
+
+		public override string ToString()
+		{
+			var number = _number;
+			var result = string.Empty;
+			var position = 0;
+
+			while (number > 0)
+			{
+				var digit = number % 10;
+				number /= 10;
+				if (digit > 0)
+				{
+					result = _digitsMap[position][digit] + result;
+				}
+				position++;
+			}
+
+			return result;
+		}
+
+		public string ToString2()
+		{
+			var mappedDigits = Digits()
+				.Select(MapDigit)
+				.ToList();
+
+			mappedDigits.Reverse();
+
+			return string.Join("", mappedDigits);
+		}
+
+		private string MapDigit(int digit, int index)
+		{
+			return (digit > 0) ? _digitsMap[index][digit] : string.Empty;
+		}
+	}
+}
---
+++ UnitTestProject1/RomanNumberShould.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumberConverter;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class RomanNumberShould
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void NotConvertInvalidNumbers()
+		{
+			var number = new RomanNumber(-1);
+		}
+
+		[TestMethod]
+		public void ConvertOneDigitNumbers()
+		{
+			Assert.AreEqual(new RomanNumber(2).ToString(), "II");
+			Assert.AreEqual(new RomanNumber(4).ToString(), "IV");
+			Assert.AreEqual(new RomanNumber(8).ToString(), "VIII");
+		}
+
+		[TestMethod]
+		public void ConvertTwoDigitsNumbers()
+		{
+			Assert.AreEqual(new RomanNumber(57).ToString(), "LVII");
+			Assert.AreEqual(new RomanNumber(10).ToString(), "X");
+			Assert.AreEqual(new RomanNumber(63).ToString(), "LXIII");
+			Assert.AreEqual(new RomanNumber(99).ToString(), "XCIX");
+		}
+
+		[TestMethod]
+		public void ConvertThreeDigitsNumbers()
+		{
+			Assert.AreEqual(new RomanNumber(123).ToString(), "CXXIII");
+			Assert.AreEqual(new RomanNumber(444).ToString(), "CDXLIV");
+			Assert.AreEqual(new RomanNumber(686).ToString(), "DCLXXXVI");
+			Assert.AreEqual(new RomanNumber(999).ToString(), "CMXCIX");
+		}
+
+		[TestMethod]
+		public void ConvertFourDigitsNumbers()
+		{
+			Assert.AreEqual(new RomanNumber(1999).ToString(), "MCMXCIX");
+			Assert.AreEqual(new RomanNumber(2345).ToString(), "MMCCCXLV");
+			Assert.AreEqual(new RomanNumber(1000).ToString(), "M");
+			Assert.AreEqual(new RomanNumber(3999).ToString(), "MMMCMXCIX");
+		}
+	}
+}
